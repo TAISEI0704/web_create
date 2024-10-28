102bc54dc632c130444ea4ed4c0af1af79d7e628
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2024-10-28 16:06:27 +0900

    ヘッダー~section1までのレスポンシブ完了

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
+// ハンバーガーメニューのスクロールを抑制する
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        document.body.classList.add('no-scroll'); // スクロール禁止
+        // console.log('check');
+    } else {
+        document.body.classList.remove('no-scroll'); // スクロール解除
+        // console.log('uncheck');
+    }
+});
+
+
+
+
+
+// workページ
 const currentPath = window.location.pathname;
 
 const links = document.querySelectorAll('#work-nav ul li a');
 
 links.forEach(link => {
     const linkPath = new URL(link.getAttribute('href'), window.location.origin).pathname;
-
     if (linkPath === currentPath) {
         link.classList.add('active');
     }
