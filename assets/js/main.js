9b95050f1e2177ff22a75a2245115df754a4e704
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2024-10-31 14:26:10 +0900

    Workレスポンシブ完成

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,16 @@
 // ハンバーガーメニューのスクロールを抑制する
 const checkbox = document.getElementById('checkbox');
-
 checkbox.addEventListener('change', () => {
     if (checkbox.checked) {
         document.body.classList.add('no-scroll'); // スクロール禁止
-        // console.log('check');
     } else {
         document.body.classList.remove('no-scroll'); // スクロール解除
-        // console.log('uncheck');
     }
 });
 
 //sectionのフェードイン
 document.addEventListener('DOMContentLoaded', function() {
     const elements = document.querySelectorAll('.fadeIn');
-
     function checkVisibility() {
         elements.forEach(element => {
             const rect = element.getBoundingClientRect();
@@ -23,22 +19,20 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
-
     window.addEventListener('scroll', checkVisibility);
-    checkVisibility(); // 初期チェック
+    checkVisibility();
 });
 
-
-
-
 // workページ
-const currentPath = window.location.pathname;
-
+const currentPath = window.location.pathname.replace(/\/$/, "").replace(/\.\w+$/, "");
 const links = document.querySelectorAll('#work-nav ul li a');
-
 links.forEach(link => {
-    const linkPath = new URL(link.getAttribute('href'), window.location.origin).pathname;
+    const linkPath = new URL(link.getAttribute('href'), window.location.origin).pathname.replace(/\/$/, "").replace(/\.\w+$/, "");
     if (linkPath === currentPath) {
         link.classList.add('active');
     }
+});
+
+document.getElementById("work-nav-select").addEventListener("change", function() {
+    window.location.href = this.value;
 });
